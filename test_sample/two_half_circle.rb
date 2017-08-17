d918e3f6cc604b595b9b335b878a6f8a714b2648
He Yong <yhe@vip.sina.com>
2017-08-17 09:57:51 +0800

    Create Ruby file to generate test samples in 2 half-circle areas. The first file in the git project.

---
+++ test_sample/two_half_circle.rb
@@ -0,0 +1,13 @@
+# /usr/bin/ruby -w
+
+# Create 2000 pairs of dots in two half-circle areas
+
+BEGIN {
+    print("Start to generate samples in two half-circle areas.\n");
+}
+
+END {
+    print("Program end.\n");
+}
+__END__
+2017.08.17
