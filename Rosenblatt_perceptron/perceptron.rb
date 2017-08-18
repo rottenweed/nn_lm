0196c5c7d8a5e13d48411b8d35e200e211b9f1b0
He Yong <yhe@vip.sina.com>
2017-08-18 12:01:16 +0800

    the Preceptron is OK.

--- Rosenblatt_perceptron/perceptron.rb
+++ Rosenblatt_perceptron/perceptron.rb
@@ -4,21 +4,54 @@
 # Process dots in two half-circle area
 
 class Perceptron
-    attr_accessor(:delta);  # ration in step-to-step delta
+    attr_accessor(:delta);  # ration for step-to-step delta
     attr_accessor(:xn);     # input x1 ~ xn
+    attr_accessor(:wn);     # rations for xn
+    attr_accessor(:b);      # offset
 
-    def initialize(n = 2, delta = 0.5, b = 0)
+    # Parameters:
+    #   n: dimension.
+    #   delta: ration for step-to-step delta.
+    #   b: offset.
+    # Object variable:
+    #   wn: rations for every input
+    def initialize(n = 2, delta = 0.5, b = 0.0)
+        raise("delta must >0.") if (delta < 0.0);
         @n = n;
         @delta = delta;
-        @wn = Array.new(@n, 0);
+        @xn = Array.new(@n, 0.0);
+        @wn = Array.new(@n, 0.0);
         @b = b;
+        print(@xn, @wn, "\n");
     end
 
-    def value()
+    # calculation for the inputs and offset
+    def y()
+        val = @b;
         @n.times {|i|
-            val += @xn[i] * @wn[i];
+            val +=  @wn[i] * @xn[i];
         }
-        val += @b;
+        return val;
+    end
+
+    # output sign function
+    def output()
+        (y() >= 0) ? 1 : 0;
+    end
+
+    # train the neuron
+    def train(expect)
+        if((expect == 1) && (output == 0))
+            @n.times {|i|
+                wn[i] += @delta * xn[i];
+            }
+            @b += @delta;   # as w0, x0
+        elsif((expect == 0) && (output == 1))
+            @n.times {|i|
+                wn[i] -= @delta * xn[i];
+            }
+            @b -= @delta;   # as w0, x0
+        end
     end
 
     def to_s()
--- Rosenblatt_perceptron/test_perceptron.rb
+++ Rosenblatt_perceptron/test_perceptron.rb
@@ -5,16 +5,61 @@
 
 BEGIN {
     print("Test Rosenblatt Perceptron.\n");
+    CSV = File.open("wn.csv", "w");
 }
 
 require("../test_sample/two_half_circle.rb");
 require("./perceptron.rb");
 
-neuron = Perceptron.new(3, 0.7, 5);
+neuron = Perceptron.new(2, 0.5, 0);
 print(neuron.to_s, "\n");
+
+right_cnt = 0;
+error_cnt = 0;
+10000.times {|i|
+    dot = random_dot_pair_half_circle(10.0, 6.0, 1.0);
+    neuron.xn = dot[0];
+    if(neuron.output == 1)
+        right_cnt += 1;
+    else
+        error_cnt += 1;
+    end
+    neuron.train(1);
+    neuron.xn = dot[1];
+    if(neuron.output == 0)
+        right_cnt += 1;
+    else
+        error_cnt += 1;
+    end
+    neuron.train(0);
+    CSV.print("#{neuron.wn[0]},#{neuron.wn[1]},#{neuron.b}\n");
+}
+    print("End: ", neuron.to_s, "\n");
+    print("Right: #{right_cnt}, Error: #{error_cnt}\n");
+
+right_cnt = 0;
+error_cnt = 0;
+2000.times {|i|
+    dot = random_dot_pair_half_circle(10.0, 6.0, 1.0);
+    neuron.xn = dot[0];
+    if(neuron.output == 1)
+        right_cnt += 1;
+    else
+        error_cnt += 1;
+    end
+    neuron.xn = dot[1];
+    if(neuron.output == 0)
+        right_cnt += 1;
+    else
+        error_cnt += 1;
+    end
+}
+    print("Right: #{right_cnt}, Error: #{error_cnt}\n");
+
 
 END {
     print("Program end.\n");
+    CSV.close;
 }
 __END__
 2017.08.17
