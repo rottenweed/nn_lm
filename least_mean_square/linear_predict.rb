e1b54ced05e25b9b34d68be2658672565da9b254
He Yong <yhe@vip.sina.com>
2017-08-31 15:02:17 +0800

    Create the frame of test LMS experiment: Linear Prediction

--- least_mean_square/linear_predict.rb
+++ least_mean_square/linear_predict.rb
@@ -8,8 +8,15 @@ class Predict_Test < Adaptive_Filter
     end
 end
 
-print("Test method inherit\n");
+BEGIN {
+    print("Linear predict of LMS.\n");
+}
 
 neuron = Predict_Test.new(2);
 neuron.feedback;
 
+END {
+    print("Program End.\n");
+}
+__END__
+2017.08.31
