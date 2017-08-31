c9159e28771de60e7bf890582f3020d2a68ebd94
He Yong <yhe@vip.sina.com>
2017-08-31 11:56:47 +0800

    Start to LMS algorithm

---
+++ least_mean_square/adaptive_filter.rb
@@ -0,0 +1,55 @@
+#! /usr/bin/ruby -w
+
+# Adaptive Filter.
+# A linear neuron with feadback
+class Adaptive_Filter
+    attr_accessor(:xn);     # input x1 ~ xn
+    attr_accessor(:wn);     # rations for xn
+
+    # Parameters:
+    #   n:  input dimensionality.
+    #   xn: input x, x1 as 1.0 forever.
+    #   di: desired response (target), input from external system.
+    # Object variable:
+    #   wn: rations for every input.
+    def initialize(n = 2)
+        @n = n;
+        @xn = Array.new(@n, 0.0);
+        @wn = Array.new(@n, 0.0);
+        @di = 0;
+    end
+
+    # setup wn value from external
+    def set_w(wn)
+        @wn = wn;
+    end
+
+    # sum calculation for the inputs
+    def y()
+        val = 0.0;
+        @n.times {|i|
+            val +=  @wn[i] * @xn[i];
+        }
+        return val;
+    end
+
+    # signum output
+    def signum()
+        (y() >= 0) ? 1.0 : -1.0;
+    end
+
+    # error between input desired response and sum,
+    # used in feedback loop.
+    def e()
+        di - y();
+    end
+
+    # virtual feedback method
+    def feedback()
+        raise("Not defined method for feedback!");
+    end
+
+end
+
+__END__
+2017.08.30
