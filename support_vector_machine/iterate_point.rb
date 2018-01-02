76c48f0cdeb07d55568577d234cf0bbb9bfdac27
yhe <yhe@sina.com>
2018-01-02 22:34:39 +0800

    simulate SMO

--- support_vector_machine/iterate_point.rb
+++ support_vector_machine/iterate_point.rb
@@ -1,7 +1,36 @@
 #! /usr/bin/ruby -w
-# Iterate wn/b in points
+# Iterate a(n) in dual question
 
-print("Use points one by one in SVM for iteration.\n")
+require 'matrix'
+print("Solve dual question in iterate loop.\n")
 
-point_pos = [];
-point_neg = [];
+def w()
+    val = 0;
+    4.times {|i| val += A[i];}
+    4.times {|m|
+        m.upto(3) {|n|
+            val -= Kij[m, n] * A[m] * A[n];
+        }
+    }
+    return val;
+end
+
+# Point positon
+X = [[-1, 1], [1, 1], [0, -1], [0, 2]];
+# Value
+D = [1, 1, -1, 1];
+# Kernel as x(i)*x(j)
+Kij = Matrix.build(4, 4) {|i, j|
+    if(i < j)
+        2 * (X[i][0] * X[j][0] + X[i][1] * X[j][1]) * D[i] * D[j];
+    elsif(i == j)
+        (X[i][0] * X[j][0] + X[i][1] * X[j][1]) * D[i] * D[j];
+    else
+        0;
+    end
+};
+# Lagrange multiple number
+A = [0.5, 0.5, 1.0, 0.0];
+
+print(Kij, "\n");
+print w;
