f112ab6540d5b02232852eace08e8d44a4c6fbf2
He Yong <yhe@vip.sina.com>
2018-01-10 20:22:31 +0800

    Fix some bugs. Still bug exists.

--- support_vector_machine/iterate_point.rb
+++ support_vector_machine/iterate_point.rb
@@ -5,13 +5,13 @@ require 'matrix'
 print("Solve dual question in iterate loop.\n")
 
 point_cnt = 4;
-MAX_A_VALUE = nil;
+MAX_A_VALUE = 1e10;
 # Point positon
 X = [[-1, 1], [1, 1], [0, -1], [0, 2]];
 # Value
 D = [1, 1, -1, 1];
 # Kernel as x(i)*x(j)
-k = Matrix.build(4, 4) {|i, j|
+k = Matrix.build(point_cnt, point_cnt) {|i, j|
     X[i][0] * X[j][0] + X[i][1] * X[j][1];
 };
 # Lagrange multiple number
@@ -19,19 +19,20 @@ a = [0.5, 0.5, 1.0, 0.0];
 
 print(k, "\n");
 
-n1 = 0;
-n2 = 1;
+n1 = 1;
+n2 = 2;
 aidi_else = 0;
 point_cnt.times {|i|
     if((i != n1) && (i != n2))
         aidi_else += a[i] * D[i];
     end
 }
-if(D[n1] == D[n2])
+if(D[n1] * D[n2] > 0)
     a1_min = 0;
-    a1_max = - aidi_else;
+    a1_max = - aidi_else * D[n1];
+    raise("Error! a1_max < 0!") if(a1_max < 0);
 else
-    a1_min = [0, - aidi_else].max;
+    a1_min = [0, - aidi_else * D[n1]].max;
     a1_max = MAX_A_VALUE;
 end
 
@@ -44,6 +45,9 @@ point_cnt.times {|i|
 }
 a1 *= D[n1];
 a1 /= k[n1, n1] + k[n2, n2] - 2 * k[n1, n2];
-a2 = -a1 * D[n1] * D[n2] - aidi_else;
+# a1 value limit
+a1 = a1_min if(a1 < a1_min);
+a1 = a1_max if(a1 > a1_max);
+a2 = -a1 * D[n1] * D[n2] - aidi_else * D[n2];
 
 print "#{aidi_else}, #{a1_min}, #{a1_max}, #{a1}, #{a2}\n";
