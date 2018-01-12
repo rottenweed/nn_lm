cbcae4cec4588994b9bbc031a6b9f84b489ad76d
He Yong <yhe@vip.sina.com>
2018-01-12 18:14:17 +0800

    The algorithm is right, while the bug is on the test sample.

--- support_vector_machine/iterate_point.rb
+++ support_vector_machine/iterate_point.rb
@@ -5,7 +5,7 @@ require 'matrix'
 print("Solve dual question in iterate loop.\n")
 
 point_cnt = 4;
-MAX_A_VALUE = 1e10;
+MAX_A_VALUE = 1E10;
 # Point positon
 X = [[-1, 1], [1, 1], [0, -1], [0, 2]];
 # Value
@@ -15,12 +15,10 @@ k = Matrix.build(point_cnt, point_cnt) {|i, j|
     X[i][0] * X[j][0] + X[i][1] * X[j][1];
 };
 # Lagrange multiple number
-a = [0.5, 0.5, 1.0, 0.0];
+a = [0.25, 0.25, 0.5, 0.0];
 
-print(k, "\n");
-
-n1 = 1;
-n2 = 2;
+n1 = 2;
+n2 = n1 + 1;
 aidi_else = 0;
 point_cnt.times {|i|
     if((i != n1) && (i != n2))
@@ -49,5 +47,7 @@ a1 /= k[n1, n1] + k[n2, n2] - 2 * k[n1, n2];
 a1 = a1_min if(a1 < a1_min);
 a1 = a1_max if(a1 > a1_max);
 a2 = -a1 * D[n1] * D[n2] - aidi_else * D[n2];
+a[n1] = a1;
+a[n2] = a2;
 
-print "#{aidi_else}, #{a1_min}, #{a1_max}, #{a1}, #{a2}\n";
+print "#{a}\n";
