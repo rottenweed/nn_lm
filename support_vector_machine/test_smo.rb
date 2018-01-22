b24ebb5af5e48b54f502d81054e170742ffb4a9f
yhe <yhe@sina.com>
2018-01-22 17:21:15 +0800

    Next trap in SMO

--- support_vector_machine/test_smo.rb
+++ support_vector_machine/test_smo.rb
@@ -4,10 +4,10 @@
 require './smo'
 
 SMO.MAX_LIMIT_C = 1E6;
-point_cnt = 6;
+point_cnt = 5;
 dimension = 2;
-x = [[-1, 1], [1, 1], [0, -1], [0, 2], [1, 2], [-2, -2]];
-d = [1, 1, -1, 1, 1, -1];
+x = [[-1, 1], [1, 1], [0, -1], [0, 2], [2, -2]];
+d = [1, 1, -1, 1, -1];
 k = Matrix.build(point_cnt, point_cnt) {|i, j|
     k_ij = 0.0;
     dimension.times {|m|
@@ -17,7 +17,8 @@ k = Matrix.build(point_cnt, point_cnt) {|i, j|
 };
 
 print "Point count = #{SMO.init_point(point_cnt, d, k)}\n";
-SMO.a = [0.25, 0.25, 0.5, 0, 0, 0];
+#SMO.a = [0.25, 0.25, 0.5, 0, 0];
+#SMO.a = [1, 0, 1, 0, 0];
 cycle = SMO.iterate;
 print "Iterate times: #{cycle}\n";
 a = SMO.result;
