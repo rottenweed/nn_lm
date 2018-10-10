9a8a8a7f29103b4c89b788a22375befb73f6e18f
yhe <yhe@sina.com>
2018-10-10 21:36:30 +0800

    Regular Theory

---
+++ regular_theory/lms_ssl.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/ruby -w
+
+print("Test two half circle in Semi-Supervised Learning.\n");
+
+# config samples generator
+require("../test_sample/two_half_circle.rb");
+CIRCLE_R = 10.0;
+CIRCLE_WIDTH = 6.0;
+CIRCLE_DIS = -1.0;
