30460f9548a7fe4789d7c2d22e195a363614b16d
yhe <yhe@sina.com>
2018-10-29 21:41:15 +0800

    Modify for semi-supervise

--- regular_theory/lms_ssl.rb
+++ regular_theory/lms_ssl.rb
@@ -1,5 +1,11 @@
 #! /usr/bin/ruby -w
 
+def val_f(x)
+    # continuous version: Math.tanh(x)
+    val_f = (x == 0) ? 0.0 :
+            (x > 0) ? 1.0 : -1.0; 
+end
+
 print("Test two half circle in Semi-Supervised Learning.\n");
 
 # config samples generator
@@ -8,3 +14,15 @@ CIRCLE_R = 10.0;
 CIRCLE_WIDTH = 6.0;
 CIRCLE_DIS = 1.0;
 
+# generate points in two half-circles for training
+TRAIN_CNT = 10;
+SUPERVISE_CNT = 4;
+train_point_xy = [];
+train_point_d = [];
+(TRAIN_CNT / 2).times {|i|
+    point_pair = random_dot_pair_half_circle(CIRCLE_R, CIRCLE_WIDTH, CIRCLE_DIS);
+    train_point_xy << point_pair[0];
+    train_point_d << 1.0;
+    train_point_xy << point_pair[1];
+    train_point_d << -1.0;
+}
